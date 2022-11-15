3c61c69ad23824af70ba6c554abe76ae921d0c66
joberg95 <joakim.berglind@gmail.com>
2022-11-15 21:45:29 +0100

    setup post feed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,70 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import toast from "react-hot-toast"
-
+import toast from "react-hot-toast";
+import { firestore, fromMillis, postToJSON } from "../../lib/firebase";
+import { useState } from "react";
 import Loader from "../components/Loader";
+import PostFeed from "../components/PostFeed";
 
-export default function Home() {
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
+    <main>
+      <PostFeed posts={posts} />
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load More</button>
+      )}
+      <Loader show={loading} />
+      {postsEnd && "You have reached the end!"}
       <Loader show />
-      <button onClick={() => toast.success("hello toast!")}>
-        Toast me
-      </button>
-    </div>
+      
+      <button onClick={() => toast.success("hello toast!")}>Toast me</button>
+    </main>
   );
 }
